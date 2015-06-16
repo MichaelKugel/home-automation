c6be94f39a3fe06a406157527dad675a04e217ae
Michael Kugel <mkugel@mkugels-MacBook-Pro.local>
2015-06-16 16:30:12 +0100

    Implemented de- and multiplexer

--- 3rd-party/demultiplexer.py
+++ 3rd-party/demultiplexer.py
@@ -1,32 +1,43 @@
+# HAS PAHO DEMULTIPLEXER
+# (C) 2015 mkugel
+
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time
 
+host = "10.37.85.223"
+in_topic = "control/service/security/access/general"
+out_topics = ["control/service/security/access/frontdoor", "control/service/security/access/backdoor", "control/service/security/access/sidedoor"]
 
 # The callback for when the client receives a CONNACK response from the server.
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-    client.subscribe("service/safety/smoke-alarm")
+
+    client.subscribe(in_topic)
 
 # The callback for when a PUBLISH message is received from the server.
+
 def on_message(client, userdata, msg):
-    msgs = [{'topic':"service/safety/gateway-1", 'payload':"relayed"},
-    ("service/safety/gateway-1", str(msg.payload), 0, False)]
-    publish.multiple(msgs, hostname="192.168.1.100")
+
+    for topic in out_topics:
+        print topic
+        msgs = [{'topic':topic, 'payload':"demultiplexed"}, (topic, str(msg.payload), 0, False)]
+        publish.multiple(msgs, hostname=host)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("10.37.85.223", 1883, 60)
+client.connect(host, 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
+
 client.loop_forever()
 
