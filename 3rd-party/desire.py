7967f23a5388b0fe9cdf6da7813d1d7ccc2fe846
kugelmeister <kugelmeister@hotmail.com>
2015-06-24 20:34:15 +0100

    Added prompt echo to desire script

--- 3rd-party/desire.py
+++ 3rd-party/desire.py
@@ -6,6 +6,8 @@ import time
 from pexpect import pxssh as pxssh
 import pexpect
 
+containername='fourthpx'
+
 # print 'Number of arguments:', len(sys.argv), 'arguments.'
 # print 'Argument List:', str(sys.argv[1])
 if (str(sys.argv[1]) in ['-h', '--help', '?']):
@@ -18,12 +20,19 @@ if (str(sys.argv[1]) == 'desire.yaml'):
   print "Attempt 1"
  #  os.system("expect ../bash/expect/expect.sh 10.37.85.223 root root ' -it --name firstexpect /mkugel/archmosqextpysensor /bin/bash'") 
   
+  dockercommand = 'docker run -it --name '+containername+' mkugel/archmosqextpysensor /bin/bash'
   p = pxssh.pxssh()
   p.login('10.37.85.223', 'root', 'root')
-  p.sendline('docker run -it --name firstpx mkugel/archmosqextpysensor /bin/bash')
-
+  # p.sendline('docker run -it --name secondpx mkugel/archmosqextpysensor /bin/bash')
+  p.sendline(dockercommand)
   
   time.sleep(1)
   print "Verifying desired state"
   time.sleep(1)
-  print "Confirmed"
+  p.sendline('docker ps')
+  p.prompt()
+  output = p.before
+  if(containername in output):
+    print "Confirmed"
+  else:
+    print "Could NOT confirm if "+containername+" was launched."
