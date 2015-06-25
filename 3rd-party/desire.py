3926bdda79289db9f381501a99fac187a33e1439
Michael Kugel <mkugel@mkugels-MacBook-Pro.local>
2015-06-25 12:30:37 +0100

    Added more code for desire

--- 3rd-party/desire.py
+++ 3rd-party/desire.py
@@ -5,26 +5,73 @@ import sys
 import time
 from pexpect import pxssh as pxssh
 import pexpect
+import yaml
 
-containername='fourthpx'
+# containername='testpx'
 
 # print 'Number of arguments:', len(sys.argv), 'arguments.'
 # print 'Argument List:', str(sys.argv[1])
-if (str(sys.argv[1]) in ['-h', '--help', '?']):
-  print "Author: mkugel"
-  print "Help (-h)"
-  print "Usage: python <this.py> <desire.conf>"
+
 if (str(sys.argv[1]) == 'desire.yaml'):
   print "Attempting to reach desired state"
   time.sleep(1)
   print "Attempt 1"
+
+# YAML parsing
+
+  print "Opening yaml file"
+  f = open('desire.yaml')
+  print "Parsing Yaml file"
+  desire_dictionary = yaml.load(f)
+  print "Closing YAML file"
+  f.close()
+
+  #with open("desire.yaml", 'r') as stream:
+  #  print(yaml.load(stream))
+
+# Variable Assignments
+
+  has_host = desire_dictionary['infrastructure']['host'][0]
+  print "Assigning host to be:"
+  print(desire_dictionary['infrastructure']['host'][0])
+
+  containername = desire_dictionary['infrastructure']['host'][0]
+  print "Assgning container name to be:"
+  print(desire_dictionary['container']['name'][0])
+
+  codefolder = desire_dictionary['code']['folder'][0]
+  print "Assigning code folder to be:"
+  print(desire_dictionary['code']['folder'][0])
+
+  codesource = desire_dictionary['code']['source'][0] 
+  print "Assigning code source file to be:"
+  print(desire_dictionary['code']['source'][0])
+
+  codecommand = desire_dictionary['code']['command'][0]
+  print "Assigning code interpreter to be:"
+  print(desire_dictionary['code']['command'][0])
+
+  print "Assigning topics (MOM Interfaces - not implemented yet!)
+
  #  os.system("expect ../bash/expect/expect.sh 10.37.85.223 root root ' -it --name firstexpect /mkugel/archmosqextpysensor /bin/bash'") 
+
+# Remote Execution of Docker command using pexpect via ssh
   
-  dockercommand = 'docker run -it --name '+containername+' mkugel/archmosqextpysensor /bin/bash'
+  dockercommand = 'docker run -it --name '+containername+' -v '+codefolder+':/root/code/3rd-party mkugel/archmosqextpysensor '+codecommand+' /root/code/3rd-party/'+codesource
+
+  print "The docker command executed on the remote host is:"
+  print(dockercommand)
+
+  # dockercommand = 'docker run -it --name '+containername+' mkugel/archmosqextpysensor /bin/bash'
+
   p = pxssh.pxssh()
-  p.login('10.37.85.223', 'root', 'root')
+  p.login(has_host, 'root', 'root')
+
   # p.sendline('docker run -it --name secondpx mkugel/archmosqextpysensor /bin/bash')
+
   p.sendline(dockercommand)
+
+# Verification of container deployment
   
   time.sleep(1)
   print "Verifying desired state"
@@ -36,3 +83,15 @@ if (str(sys.argv[1]) == 'desire.yaml'):
     print "Confirmed"
   else:
     print "Could NOT confirm if "+containername+" was launched."
+
+  print "Running Tests for container..."
+  time.sleep(1)
+  print "ALOK"
+
+elif (str(sys.argv[1]) in ['-h', '--help', '?']):
+  print "Author: mkugel"
+  print "Help (-h)"
+  print "Usage: python <this.py> <desire.conf>"
+
+else:
+  print "No arguments provided. Try -h for help"
